08cf2327001029554dcbf2eacbbf65ce30134b63
Jordan Rousseau <jrousseau@wdtinc.com>
2016-08-16 09:16:07 -0500

    remove unnecessary comments

--- src/renderers/google/ImageMapType.Skywise.js
+++ src/renderers/google/ImageMapType.Skywise.js
@@ -20,11 +20,8 @@ SkywiseImageMapType.prototype.getNormalizedCoord = function getNormalizedCoord(c
   var y = coord.y;
   var x = coord.x;
 
-  // tile range in one direction range is dependent on zoom level
-  // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
   var tileRange = 1 << zoom;
 
-  // don't repeat across y-axis (vertically)
   if (y < 0 || y >= tileRange) {
     return null;
   }
