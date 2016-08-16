ca163b5aa7904581cdb8b5727c26ff2fdc236e64
Jordan Rousseau <jrousseau@wdtinc.com>
2016-08-16 08:34:42 -0500

    remove ignore for keys

---
+++ dist/keys.js
@@ -0,0 +1,7 @@
+'use strict';
+window.demo_keys = {
+  skywise_app_id: "{3_SCALE_APP_ID}",
+  skywise_token: "{3_SCALE_TOKEN}",
+  google_maps_key: "{GOOGLE_MAPS_KEY}",
+  mapbox_access_token: "{MAPBOX_ACCESS_TOKEN}"
+};
